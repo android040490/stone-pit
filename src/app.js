bf2b7215da089fd129b31dc9c3292c7ca72b7873
Pypkin <apalkov040490@gmail.com>
2018-02-13 19:52:13 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,59 @@
+import "./styles/main.sass";
+import * as jQuery from "jquery";
+import "popper.js";
+import "bootstrap";
+
+var slider = $(".slide-show_section") ;
+$(function () {
+    $(window).resize(function () {
+
+        if (slider.width() < 1200 & slider.css("display") != "none") {
+
+            $('.wrapper').css('max-width', $('.slide-show_section').css('width'))
+        }
+
+        else {
+            $('.wrapper').css('max-width', '1200px');
+        }
+    });
+
+    $(window).resize();
+
+    //  Page navigation
+
+    $(".navigation ul").on("click","a", function (event) {
+
+        event.preventDefault();
+
+        var id  = $(this).attr('href');
+
+        var topSize = $(id).offset().top;
+
+        $('body,html').animate({scrollTop: topSize}, 500);
+
+    });
+
+    // modal-window
+
+    $("#modal-win-active, .modal-window_button").click(function(){
+
+        $('.modal-window').toggleClass('modal-window-hide');
+    })
+});
+
+// google maps
+
+function initMap() {
+    var khanino = { lat: 54.211260, lng: 36.618191 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: khanino,
+        disableDefaultUI: true
+    });
+    var marker = new google.maps.Marker({
+        position: khanino,
+        map: map
+    });
+}
+    
+window.initMap = initMap;   
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+"use strict;"
+const webpack = require('webpack');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    context: path.resolve(__dirname, "./src"),
+    entry: [
+        "./app"
+        // "/node_modules/bootstrap/dist/js/bootstrap"
+    ],
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js"
+    }, //
+    watch: true,
+
+    
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.sass$/, // scss
+                loader: ExtractTextPlugin.extract({
+                    use: ["css-loader", "postcss-loader", "sass-loader"], // npm install scss-loader --save-dev
+                })
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 32768,
+                            publicPath: './',
+                            outputPath: 'images/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("main.css"),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+
+    ],
+    resolve: {
+        extensions: [".js"]
+    }
+}
